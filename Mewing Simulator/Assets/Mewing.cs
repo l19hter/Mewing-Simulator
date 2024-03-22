19d93d3a8c1bc2269fd566a39876412bec8f2104
l19hter <thebrookkey@gmail.com>
2024-03-22 20:04:54 +0300

    3
    3

--- Mewing Simulator/Assets/Mewing.cs
+++ Mewing Simulator/Assets/Mewing.cs
@@ -7,7 +7,12 @@ public class Mewing : MonoBehaviour
 {
     private void OnMouseEnter()
     {
+<<<<<<< Updated upstream
         transform.localScale += new Vector3(1, 1, 1);
         //HUI 2
+=======
+        transform.localScale = new Vector2(1, 1);
+        //HUI
+>>>>>>> Stashed changes
     }
 }
